d6977e5e392d107e0812c6e46398a257164728dc
Derek Baier <derek.m.baier@gmail.com>
2023-11-30 08:36:29 -0500

    Added WinPointAutonomous

--- src/Autonomous.py
+++ src/Autonomous.py
@@ -36,13 +36,21 @@ class NothingAutonomous(AutonomousRoutine):
 
 class ScoringAutonomous(AutonomousRoutine):
     def __init__(
-        self, log_object, drivetrain, intake, catapult, terminal, startup_position
+        self,
+        log_object,
+        drivetrain,
+        intake,
+        catapult,
+        wings,
+        terminal,
+        startup_position,
     ):
         super().__init__(log_object)
         self.terminal = terminal
         self.drivetrain = drivetrain
         self.intake = intake
         self.catapult = catapult
+        self.wings = wings
         self.drivetrain.current_position = startup_position
         self.drivetrain.rotation_PID.setpoint = self.drivetrain.current_direction_rad
         self.drivetrain.target_position = self.drivetrain.current_position
@@ -93,9 +101,9 @@ class ScoringAutonomous(AutonomousRoutine):
         self.intake.stop()
         self.drivetrain.turn_to_face_heading_rad(math.radians(-180))
 
-        self.drivetrain.forward(20, 0.8)
+        self.drivetrain.forward(20, 1)
         self.intake.spit_out()
-        self.drivetrain.forward(20, 0.8)
+        self.drivetrain.forward(20, 1)
 
         self.log("Done")
         self.drivetrain.rotation_PID.setpoint = self.drivetrain.current_direction_rad
@@ -109,13 +117,21 @@ class ScoringAutonomous(AutonomousRoutine):
 
 class SabotageAutonomous(AutonomousRoutine):
     def __init__(
-        self, log_object, drivetrain, intake, catapult, terminal, startup_position
+        self,
+        log_object,
+        drivetrain,
+        intake,
+        catapult,
+        wings,
+        terminal,
+        startup_position,
     ):
         super().__init__(log_object)
         self.terminal = terminal
         self.drivetrain = drivetrain
         self.intake = intake
         self.catapult = catapult
+        self.wings = wings
         self.drivetrain.current_position = startup_position
         self.drivetrain.rotation_PID.setpoint = self.drivetrain.current_direction_rad
         self.drivetrain.target_position = self.drivetrain.current_position
@@ -153,6 +169,67 @@ class SabotageAutonomous(AutonomousRoutine):
         self.drivetrain.forward(40, 0.8)
         wait(500, MSEC)
         self.intake.stop()
+
+        self.log("Done")
+        self.drivetrain.rotation_PID.setpoint = self.drivetrain.current_direction_rad
+        self.drivetrain.clear_direction_PID_output()
+        self.drivetrain.target_position = self.drivetrain.current_position
+
+        self.drivetrain.stop()
+
+        self.log_object.exit()
+
+
+class WinPointAutonomous(AutonomousRoutine):
+    def __init__(
+        self,
+        log_object,
+        drivetrain,
+        intake,
+        catapult,
+        wings,
+        terminal,
+        startup_position,
+    ):
+        super().__init__(log_object)
+        self.terminal = terminal
+        self.drivetrain = drivetrain
+        self.intake = intake
+        self.catapult = catapult
+        self.wings = wings
+        self.drivetrain.current_position = startup_position
+        self.drivetrain.rotation_PID.setpoint = self.drivetrain.current_direction_rad
+        self.drivetrain.target_position = self.drivetrain.current_position
+
+    def log(self, string):
+        self.log_object.log(string + "\n")
+        self.terminal.print(string)
+
+    def run(self):
+        self.log("Starting win point autonomous")
+
+        self.drivetrain.stop()
+
+        self.drivetrain.forward(5, 0.4)
+        self.drivetrain.turn_to_face_heading_rad(math.radians(-45))
+        self.wings.left_wing_out()
+        self.drivetrain.forward(20, 0.4)
+        self.drivetrain.strafe_left(10, 0.4)
+        self.drivetrain.forward(30, 0.4)
+        self.wings.left_wing_in()
+        self.drivetrain.strafe_right(20, 0.4)
+        self.drivetrain.forward(30, 0.4)
+        self.drivetrain.turn_to_face_heading_rad(math.radians(-90))
+        self.drivetrain.forward(25, 0.5)
+        self.intake.spit_out()
+        wait(1000, MSEC)
+        self.intake.stop()
+        self.drivetrain.backwards(20, 0.4)
+        self.drivetrain.turn_to_face_heading_rad(math.radians(180))
+        self.drivetrain.forward(130, 1)
+        self.wings.right_wing_out()
+        self.drivetrain.turn_to_face_heading_rad(math.radians(200))
+        wait(500, MSEC)
 
         self.log("Done")
         self.drivetrain.rotation_PID.setpoint = self.drivetrain.current_direction_rad
@@ -214,5 +291,6 @@ class SkillsAutonomous(AutonomousRoutine):
 available_autonomous_routines = [
     ("SabotageAutonomous", SabotageAutonomous),
     ("ScoringAutonomous", ScoringAutonomous),
+    ("WinPointAutonomous", WinPointAutonomous),
     ("Nothing", NothingAutonomous),
 ]
